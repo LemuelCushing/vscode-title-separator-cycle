e43d96a6bd9982b39d812acd79f5d8c7440611df
LemuelCushing <seventhHorror@gmail.com>
2020-05-09 01:44:08 +0200

    scaffold commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import * as chars from "./characters.json"
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let info = (msg: string): Thenable<String | undefined> => vscode.window.showInformationMessage(msg)
+
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "title-separator-cycle" is now active!');
+	let registerCommand = (command: string, call: (...args: any[]) => any) => context.subscriptions.push(
+		vscode.commands.registerCommand(`title-separator-cycle.${command}`, call)
+	)
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('title-separator-cycle.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Title Separator Cycle!');
-	});
-
-	context.subscriptions.push(disposable);
+	registerCommand("changeSeparatorRandom", () => {
+		info("changeSeparatorRandom")
+	})
+	registerCommand("changeSeparatorCycle", () => {
+		info("changeSeparatorCycle")
+	})
+	registerCommand("chooseSeparator", () => {
+		info("chooseSeparator")
+	})
+	registerCommand("toggleChange", () => {
+		info("toggleChange")
+	})
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
